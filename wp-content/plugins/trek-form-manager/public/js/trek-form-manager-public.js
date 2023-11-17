fcdfd559f34bff846407d2a434b87617a96f02d5
edocev <emil.docev2000@gmail.com>
2023-11-17 13:43:43 +0200

    Remove plugin Trek Travel Form Manager (#38)
    Co-authored-by: Polina Todorova <polina@grind.bg>

--- wp-content/plugins/trek-form-manager/public/js/trek-form-manager-public.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {
-	var btn = 'input[type="submit"].gform_button';
-	$(document).on('click', btn, function(e){
-		$(btn).css('display','none');
-		$('.gform_footer').prepend('<p class="gfield_label gfield_wait">Submitting, please wait&hellip;</p>');
-	});
-});
-
