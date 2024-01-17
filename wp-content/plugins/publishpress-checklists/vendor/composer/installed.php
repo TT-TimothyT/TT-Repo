5651612179303a35825191790b73b00251cdf115
Mario Soltariyski <32837673+msoltariyski@users.noreply.github.com>
2024-01-17 17:20:21 +0200

    Blog updates (#151)
    * Add some styles - blog posts
    
    * Fix cta btns and next/prev button - single post
    
    * Add image placeholder for blog posts without image
    
    * Remove recent posts - fix
    
    * Added a function that will set featured image to all posts that do not have and a plugin that requires featured images for posts
    
    * Updated the image ID
    
    ---------
    
    Co-authored-by: Polina Todorova <polina@grind.bg>

---
+++ wp-content/plugins/publishpress-checklists/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f5c6fe9cd1a7f022ea026038bebb604eea109f5d',
+        'name' => 'publishpress/publishpress-checklists',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'publishpress/publishpress-checklists' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f5c6fe9cd1a7f022ea026038bebb604eea109f5d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
