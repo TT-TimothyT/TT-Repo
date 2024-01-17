0eb2d3006e7aa2bb10a4a07bb4de114d1315cbd8
AzureAD\TimTucker <tim_tucker@trektravel.com>
2024-01-17 11:33:14 -0600

    book trip url form parameters

--- wp-content/themes/trek-travel-theme/woocommerce/partials/pdp-dates-pricing.php
+++ wp-content/themes/trek-travel-theme/woocommerce/partials/pdp-dates-pricing.php
@@ -170,7 +170,7 @@ if( $get_child_products ){
                                 
                                 if ( !is_null($cart) && $persistent_cart_count > 0 ) {
                                     if ( isset( $formUrl ) && ! empty( $formUrl ) ) {
-                                        $button = '<a href="/'.$formUrl.'?trip='.$product->name.'" class="btn btn-primary btn-md rounded-1 dates-pricing-book-now">Book now</a>';
+                                        $button = '<a href="/'.$formUrl.'?tripname='.$product->name.'&tripdate='.$date_range.'" class="btn btn-primary btn-md rounded-1 dates-pricing-book-now">Book now</a>';
                                     } else {
                                         $button = '<button type="button" class="btn btn-primary btn-md rounded-1 dates-pricing-book-now" id="trip-booking-modal" data-bs-toggle="modal" data-bs-target="#tripBookingModal" data-form-id="'.$accordina_id.'" data-return-url="/?trip='.$product->name.'">Book now</button>';
                                     }
