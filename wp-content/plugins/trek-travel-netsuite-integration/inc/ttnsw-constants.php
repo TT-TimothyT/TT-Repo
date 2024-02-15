228daada40dd94094a96bf1c7eb797db0a7cda7c
metodiew <stanko@metodiew.com>
2024-02-15 14:40:03 +0200

    Pre-Launch: Map the sandbox with the live scripts
    Prepare the NetSuite scripts for the Production environment
    
    Asana task: https://app.asana.com/0/1205472772784381/1206579514981757

--- wp-content/plugins/trek-travel-netsuite-integration/inc/ttnsw-constants.php
+++ wp-content/plugins/trek-travel-netsuite-integration/inc/ttnsw-constants.php
@@ -1,21 +1,49 @@
 <?php
-define('TT_NS_HOST', '661527-sb2.restlets.api.netsuite.com');
-define('TT_NS_RESTLET_URL', "https://661527-sb2.restlets.api.netsuite.com/app/site/hosting/restlet.nl");
-define('TT_NS_ACCOUNT_ID', "661527_SB2");
-define('TT_NS_CONSUMER_KEY', "c488f4c7b9c29e0675151134c35ed9ab40177d73107e284fdeba7e2837ef775d");
-define('TT_NS_CONSUMER_SECRET', "53ac95afb58b5181e209a31b65403cbafa19fd054bac93ba15e2e0d139b6385e");
-define('TT_NS_TOKEN_ID', "e69b6d804a464844c0bf03c4050263a5e8115145702179eeca343a1c552c0be2");
-define('TT_NS_TOKEN_SECRET', "2e3ea493c973bff4bb808c6ab4e283ab5775d34a0471cc4cc3942266a4382bb1");
-define('TRIPS_SCRIPT_ID', '1296:2'); //214 -old ID, New ID - 1296
-define('BOOKING_SCRIPT_ID', 1298);
-define('TRIP_DETAIL_SCRIPT_ID', '1297:2'); //211 -old ID, New ID - 1297
-define('LISTS_SCRIPT_ID', '203:2');
-define('TRIP_MSG_SCRIPT_ID', 1297);
-define('USER_BOOKINGS_SCRIPT_ID', '1305:2');
-define('GET_BOOKING_SCRIPT_ID', '1304:2');
-define('GET_REGISTRATIONS_SCRIPT_ID', '1294:2');
-define('REFERRAL_SOURCE_SCRIPT_ID', '1475:2');
-define('DEFAULT_TIME_RANGE', '-12 hours'); // Define a default time range for sync, when no parameter is passed.
+ /**
+ * If we are not running the dev environment, load the Production scripts
+ */
+
+ /**
+  * @TODO: we have to update the NetSuite Production API keys and script keys once
+  * https://app.asana.com/0/1205472772784381/1206579514981757 is ready
+  */
+if ( ! defined( 'DX_DEV' ) ) {
+    define( 'TT_NS_HOST', '661527-sb2.restlets.api.netsuite.com' );
+    define( 'TT_NS_RESTLET_URL', 'https://661527-sb2.restlets.api.netsuite.com/app/site/hosting/restlet.nl');
+    define( 'TT_NS_ACCOUNT_ID', '661527_SB2');
+    define( 'TT_NS_CONSUMER_KEY', 'c488f4c7b9c29e0675151134c35ed9ab40177d73107e284fdeba7e2837ef775d');
+    define( 'TT_NS_CONSUMER_SECRET', '53ac95afb58b5181e209a31b65403cbafa19fd054bac93ba15e2e0d139b6385e');
+    define( 'TT_NS_TOKEN_ID', 'e69b6d804a464844c0bf03c4050263a5e8115145702179eeca343a1c552c0be2');
+    define( 'TT_NS_TOKEN_SECRET', '2e3ea493c973bff4bb808c6ab4e283ab5775d34a0471cc4cc3942266a4382bb1');
+    define( 'TRIPS_SCRIPT_ID', '1296:2' ); //214 -old ID, New ID - 1296
+    define( 'BOOKING_SCRIPT_ID', 1298);
+    define( 'TRIP_DETAIL_SCRIPT_ID', '1297:2' ); //211 -old ID, New ID - 1297
+    define( 'LISTS_SCRIPT_ID', '203:2' );
+    define( 'TRIP_MSG_SCRIPT_ID', 1297);
+    define( 'USER_BOOKINGS_SCRIPT_ID', '1305:2' );
+    define( 'GET_BOOKING_SCRIPT_ID', '1304:2' );
+    define( 'GET_REGISTRATIONS_SCRIPT_ID', '1294:2' );
+    define( 'REFERRAL_SOURCE_SCRIPT_ID', '1475:2' );
+    define( 'DEFAULT_TIME_RANGE', '-12 hours' ); // Define a default time range for sync, when no parameter is passed.
+} else {
+    define( 'TT_NS_HOST', '661527-sb2.restlets.api.netsuite.com' );
+    define( 'TT_NS_RESTLET_URL', 'https://661527-sb2.restlets.api.netsuite.com/app/site/hosting/restlet.nl');
+    define( 'TT_NS_ACCOUNT_ID', '661527_SB2');
+    define( 'TT_NS_CONSUMER_KEY', 'c488f4c7b9c29e0675151134c35ed9ab40177d73107e284fdeba7e2837ef775d');
+    define( 'TT_NS_CONSUMER_SECRET', '53ac95afb58b5181e209a31b65403cbafa19fd054bac93ba15e2e0d139b6385e');
+    define( 'TT_NS_TOKEN_ID', 'e69b6d804a464844c0bf03c4050263a5e8115145702179eeca343a1c552c0be2');
+    define( 'TT_NS_TOKEN_SECRET', '2e3ea493c973bff4bb808c6ab4e283ab5775d34a0471cc4cc3942266a4382bb1');
+    define( 'TRIPS_SCRIPT_ID', '1296:2' ); //214 -old ID, New ID - 1296
+    define( 'BOOKING_SCRIPT_ID', 1298);
+    define( 'TRIP_DETAIL_SCRIPT_ID', '1297:2' ); //211 -old ID, New ID - 1297
+    define( 'LISTS_SCRIPT_ID', '203:2' );
+    define( 'TRIP_MSG_SCRIPT_ID', 1297);
+    define( 'USER_BOOKINGS_SCRIPT_ID', '1305:2' );
+    define( 'GET_BOOKING_SCRIPT_ID', '1304:2' );
+    define( 'GET_REGISTRATIONS_SCRIPT_ID', '1294:2' );
+    define( 'REFERRAL_SOURCE_SCRIPT_ID', '1475:2' );
+    define( 'DEFAULT_TIME_RANGE', '-12 hours' ); // Define a default time range for sync, when no parameter is passed.
+}
 
 // Alter Query
 /*
