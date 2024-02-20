a5e890ff7b3c7b0b4bd10159bd5a94fd4ea076ea
metodiew <stanko@metodiew.com>
2024-02-20 12:27:46 +0200

    Display all simple and grouped products

--- wp-content/themes/trek-travel-theme/template-list-all-group-products.php
+++ wp-content/themes/trek-travel-theme/template-list-all-group-products.php
@@ -25,8 +25,35 @@ function only_grouped_products( ){
 
     foreach( $grouped_products->posts as $key => $gproduct  ) {
         echo $gproduct->ID . ',';
-        // wp_delete_post( $gproduct->ID ); // just trash them
     }
 }
 
+function only_simple_products( ){
+
+    $tax_query[] = array(
+        'taxonomy'  => 'product_type',
+        'field'     => 'name',
+        'terms'     => array( 'simple' ),
+    );
+
+    $args = array(
+        'post_type'         => 'product',
+        'post_status'       => 'publish',
+        'posts_per_page'    => '2000',
+        'tax_query' => $tax_query,
+    );
+
+
+    $grouped_products = new WP_Query( $args );
+
+    foreach( $grouped_products->posts as $key => $gproduct  ) {
+        echo $gproduct->ID . ' ';
+    }
+}
+
+echo '<h1>Grouped Posts</h1>';
 only_grouped_products();
+
+echo '<h1>Simple Products</h1>';
+
+only_simple_products();
