2d275178a82afd0e76d3794d870887d9744d708f
smetodiev@devrix.com (28172) <client_smetodiev@ohio-trektravel.vps.pagelyhosting.com>
2024-02-28 16:15:44 +0000

    Quick Patch: Changed the deploy on some of the live scripts

--- wp-content/plugins/trek-travel-netsuite-integration/inc/ttnsw-constants.php
+++ wp-content/plugins/trek-travel-netsuite-integration/inc/ttnsw-constants.php
@@ -15,16 +15,16 @@ if ( ! defined( 'DX_DEV' ) ) {
     define( 'TT_NS_CONSUMER_SECRET', 'cc8428d5300e645dc8aef4524a8be15187852108a33649e073637448cc585cee');
     define( 'TT_NS_TOKEN_ID', 'e785506906fafb0a8aaa8c723fd34f30bd32e4f634781313444fc866f5d1332b');
     define( 'TT_NS_TOKEN_SECRET', '8a7ec82c6bf78cd91199379d6e7548f9268c66bb2d5951a515db3caa9a00001d');
-    define( 'TRIPS_SCRIPT_ID', '1296:2' ); //214 -old ID, New ID - 1296
-    define( 'BOOKING_SCRIPT_ID', 1298);
-    define( 'TRIP_DETAIL_SCRIPT_ID', '1297:2' ); //211 -old ID, New ID - 1297
-    define( 'LISTS_SCRIPT_ID', '1299:2' );
-    define( 'TRIP_MSG_SCRIPT_ID', 1297);
-    define( 'USER_BOOKINGS_SCRIPT_ID', '1305:2' );
-    define( 'GUESTS_TO_SYNC_SCRIPT_ID', '1306:2' );
-    define( 'GET_BOOKING_SCRIPT_ID', '1304:2' );
-    define( 'GET_REGISTRATIONS_SCRIPT_ID', '1294:2' );
-    define( 'REFERRAL_SOURCE_SCRIPT_ID', '1475:2' );
+    define( 'TRIPS_SCRIPT_ID', '1296:1' ); //214 -old ID, New ID - 1296
+    define( 'BOOKING_SCRIPT_ID', 1298 );
+    define( 'TRIP_DETAIL_SCRIPT_ID', '1297:1' ); //211 -old ID, New ID - 1297
+    define( 'LISTS_SCRIPT_ID', '1299:1' );
+    define( 'TRIP_MSG_SCRIPT_ID', 1297 );
+    define( 'USER_BOOKINGS_SCRIPT_ID', '1305:1' );
+    define( 'GUESTS_TO_SYNC_SCRIPT_ID', '1306:1' );
+    define( 'GET_BOOKING_SCRIPT_ID', '1304:1' );
+    define( 'GET_REGISTRATIONS_SCRIPT_ID', '1294:1' );
+    define( 'REFERRAL_SOURCE_SCRIPT_ID', '1475:1' );
     define( 'DEFAULT_TIME_RANGE', '-12 hours' ); // Define a default time range for sync, when no parameter is passed.
 } else {
     define( 'TT_NS_HOST', '661527-sb2.restlets.api.netsuite.com' );
