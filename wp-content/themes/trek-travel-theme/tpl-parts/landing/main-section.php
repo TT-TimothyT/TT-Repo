5bd6318fb2a05010dc50e77af00417d8de36a748
TT-TimothyT <149618648+TT-TimothyT@users.noreply.github.com>
2024-03-25 10:25:01 -0500

    Ttucker hw (#296)
    * sing in register wording
    
    * pdp all months year annd styles
    
    * pdp hw and pdp dates styling wip
    
    * pdp date status styles
    
    * pdp date status styles 2
    
    * pdp navigation desktop commented js padding
    
    * pdp branch master merge
    
    * hw landing structure
    
    * hw structure and styles 1
    
    * hw landing repeater wip
    
    * htags class color
    
    * hw landing complete
    
    * nocap folder
    
    * tpl part comma
    
    * folder rename part1
    
    * folder rename part2
    
    * cta blog
    
    * cta blog ugh
    
    * hw acf fix
    
    * hw landing edits
    
    * inc img cover
    
    * acf btn manual link

--- wp-content/themes/trek-travel-theme/tpl-parts/landing/main-section.php
+++ wp-content/themes/trek-travel-theme/tpl-parts/landing/main-section.php
@@ -7,9 +7,12 @@ $main_content = get_field('main_content');
 
 $main_button = get_field('main_button');
 $btn_text = $main_button['text'];
+$btn_tf = $main_button['manual'];
 $btn_post = $main_button['link'];
+$btn_mlink = $main_button['manual_link'];
 $btn_link = get_permalink($btn_post);
 
+print_r($btn_tf);
 ?>
 
 <?php if (!empty($main_header) || !empty($main_content)): ?>
@@ -20,9 +23,15 @@ $btn_link = get_permalink($btn_post);
                     <div class="main-content">
                         <h2 class=""><?php echo esc_html($main_header); ?></h2>
                         <div class="wysiwyg text-center"><?php echo $main_content; ?></div>
-
-                        <?php if (!empty($btn_text) && !empty($btn_link)) { ?>
-                            <a class="btn btn-primary" href="<?php echo esc_url($btn_link); ?>"><?php echo $btn_text; ?></a>
+                        
+                        <?php if (!empty($btn_tf) && ($btn_tf == true)) { ?>
+                            <?php if (!empty($btn_text) && !empty($btn_mlink)) { ?>
+                                <a class="btn btn-primary" href="<?php echo esc_url($btn_mlink); ?>"><?php echo $btn_text; ?></a>
+                            <?php } ?>
+                        <?php } else { ?>
+                            <?php if (!empty($btn_text) && !empty($btn_link)) { ?>
+                                <a class="btn btn-primary" href="<?php echo esc_url($btn_link); ?>"><?php echo $btn_text; ?></a>
+                            <?php } ?>
                         <?php } ?>
                     </div>
                 </div>
