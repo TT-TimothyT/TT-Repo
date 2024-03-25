12d598d3fca1a0b530bd555c698f18017e5459b9
metodiew <stanko@metodiew.com>
2024-03-25 17:16:49 +0200

    My account: check if there is a Woo product, otherwise display an empty string

--- wp-content/themes/trek-travel-theme/woocommerce/myaccount/dashboard.php
+++ wp-content/themes/trek-travel-theme/woocommerce/myaccount/dashboard.php
@@ -195,6 +195,7 @@ $billing_country_name = WC()->countries->countries[$billing_country];
 								$guest_is_primary = isset( $order_details[0]['guest_is_primary'] ) ? $order_details[0]['guest_is_primary'] : 0;
 								$waiver_signed = isset( $order_details[0]['waiver_signed'] ) ? $order_details[0]['waiver_signed'] : false;
 								$product = wc_get_product( $product_id );
+								$product_name = ! empty( $product->get_name() ) ? $product->get_name() : '';
 								$trip_name = $trip_sdate = $trip_edate = $trip_sku = '';
 								$is_checklist_completed = tt_is_checklist_completed( $userInfo->ID, $order_id, $order_details[0]['rider_level'], $product_id, $order_details[0]['bike_id'], $guest_is_primary, $waiver_signed );
 								if( $product ){
@@ -234,7 +235,7 @@ $billing_country_name = WC()->countries->countries[$billing_country];
 									$parentTrip = tt_get_parent_trip($trip_sku);
 									$trip_link = esc_url( add_query_arg( 'order_id', $order_id, get_permalink( TREK_MY_ACCOUNT_PID ).'my-trip' ) );
 								}
-								$trips_html .= '<div class="dashboard__trip d-flex"><div class="my-upcoming-trips"><img src="'.$parentTrip['image'].'"></div><div class="w-50"><h6 class="fs-sm lh-sm fw-bold mb-1"><a href="'.$trip_link.'">'.$product->get_name().'</a></h6>';
+								$trips_html .= '<div class="dashboard__trip d-flex"><div class="my-upcoming-trips"><img src="'.$parentTrip['image'].'"></div><div class="w-50"><h6 class="fs-sm lh-sm fw-bold mb-1"><a href="'.$trip_link.'">'.$product_name.'</a></h6>';
 
 								// Check if $pa_city exists before adding it
 								if ( ! empty( $pa_city ) ) {
