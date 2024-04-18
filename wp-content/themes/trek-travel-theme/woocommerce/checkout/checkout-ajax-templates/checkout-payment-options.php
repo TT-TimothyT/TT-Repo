827fbf2c5f63422568a6df8eb0c9180d56294bf1
TT-TimothyT <149618648+TT-TimothyT@users.noreply.github.com>
2024-04-18 12:00:58 -0500

    Ttucker checkout (#336)
    full condition

--- wp-content/themes/trek-travel-theme/woocommerce/checkout/checkout-ajax-templates/checkout-payment-options.php
+++ wp-content/themes/trek-travel-theme/woocommerce/checkout/checkout-ajax-templates/checkout-payment-options.php
@@ -43,6 +43,6 @@ $pay_amount              = isset($tt_posted['pay_amount']) ? $tt_posted['pay_amo
 </div>
 <p class="mb-2">Remaining Amount Owed: <span class="fw-medium"><?php echo $remaining_amountCurr; ?></span></p>
 
-<?php if ( $remaining_amount !== 0.00) { ?>
-<p class="fs-sm lh-sm checkout-payment__gray">Our team will reach out to collect final payment prior to your trip start date.</p>
-<?php } ?>
+    <?php if ( $pay_amount !== 'full') { ?>
+        <p class="fs-sm lh-sm checkout-payment__gray">Our team will reach out to collect final payment prior to your trip start date.</p>
+    <?php } ?>
