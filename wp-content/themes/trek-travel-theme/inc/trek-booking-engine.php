2fcf2c019714551d095637cb7244fa864bf57eb5
Stanko Metodiev <stanko@metodiew.com>
2024-05-29 10:44:17 +0300

    TM Netsuite: fix addrphone error (#377)
    add shipping_phone as meta value for users, same as their main phone number
    Asana Task: https://app.asana.com/0/1205828450090907/1207298859456517/f
    
    Co-authored-by: Petyo Lazarov <plazarov@devrix.com>

--- wp-content/themes/trek-travel-theme/inc/trek-booking-engine.php
+++ wp-content/themes/trek-travel-theme/inc/trek-booking-engine.php
@@ -196,6 +196,7 @@ function insert_records_guest_bookings_cb( $order_id, $custom_user = null, $call
             // Save guest_phone as billing_phone to prevent overwriting the primary phone from TM NetSuite plugin.
             update_user_meta( $user_id, 'custentity_phone_number', $checkout_data['shipping_phone'] );
             update_user_meta( $user_id, 'billing_phone', $checkout_data['shipping_phone'] );
+            update_user_meta( $user_id, 'shipping_phone', $checkout_data['shipping_phone'] );
         }
         if( $product ){
             $trip_status = $product->get_attribute( 'pa_trip-status' );
