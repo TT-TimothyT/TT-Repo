376d49c21f95454c6be8e21c9438a12a83a97e0a
TT-TimothyT <149618648+TT-TimothyT@users.noreply.github.com>
2024-07-24 15:12:30 -0500

    Ttucker pdp update (#423)
    * sing in register wording
    
    * pdp all months year annd styles
    
    * pdp hw and pdp dates styling wip
    
    * pdp date status styles
    
    * pdp date status styles 2
    
    * pdp navigation desktop commented js padding
    
    * pdp branch master merge
    
    * hw landing structure
    
    * hw structure and styles 1
    
    * pdp copntainer sizing
    
    * container sizing breakpoint
    
    * hw landing repeater wip
    
    * htags class color
    
    * hw landing complete
    
    * nocap folder
    
    * tpl part comma
    
    * folder rename part1
    
    * folder rename part2
    
    * cta blog
    
    * cta blog ugh
    
    * hw acf fix
    
    * pdp itinerary icons wip
    
    * pdp itinerary icons complete
    
    * pdp and itinerary hw activity version changes complete
    
    * pdp and itinerary booking form updates
    
    * hw landing edits
    
    * inc img cover
    
    * PLP hw badge and hike icon
    
    * acf btn manual link
    
    * acf btn manual link fix
    
    * acf btn manual link fixy
    
    * clean print
    
    * print goaway
    
    * hide view bikes for all but biking
    
    * date space between
    
    * desktop archive activity tax and show dates only in their starting month
    
    * foreach badge
    
    * activity tax capital
    
    * back to og wip
    
    * Hiking and walking: display the green badge if there is a Hiking in the badges
    
    * Hiking and walking: update the icon based on the trip activity
    
    * past coming
    
    * re-register typo
    
    * starting price not include private status
    
    * no print
    
    * m sync
    
    * no dupe entry
    
    * bookable private date on private pdp
    
    * mobile icon plp fix
    
    * mobile icon plp fixed
    
    * pdp no date pro race
    
    * pdp no dates
    
    * pdp no dates class
    
    * sidebar nav
    
    * other as companion
    
    * itinerary days more than 13
    
    * rides guides fix
    
    * pdp side nav fixes
    
    * pdp scroll
    
    * no horizontal
    
    * weather metric
    
    * imp and metric
    
    * responsive weather
    
    ---------
    
    Co-authored-by: smetodiev@devrix.com (28172) <client_smetodiev@ohio-trektravel.vps.pagelyhosting.com>

--- wp-content/themes/trek-travel-theme/woocommerce/partials/pdp-weather.php
+++ wp-content/themes/trek-travel-theme/woocommerce/partials/pdp-weather.php
@@ -14,72 +14,77 @@ if (!empty($tripWeather)):
             <h5 class="fw-semibold">Weather</h5>
             <p class="fw-normal fs-md lh-md">Average monthly temperature and precipitation</p>
             <div class="container pdp-weather__weather-table">
-                <table class="table table-striped">
-                    <thead class="table-dark">
-                        <tr>
-                            <th scope="col"></th>
-                            <th scope="col">Jan</th>
-                            <th scope="col">Feb</th>
-                            <th scope="col">Mar</th>
-                            <th scope="col">Apr</th>
-                            <th scope="col">May</th>
-                            <th scope="col">Jun</th>
-                            <th scope="col">Jul</th>
-                            <th scope="col">Aug</th>
-                            <th scope="col">Sep</th>
-                            <th scope="col">Oct</th>
-                            <th scope="col">Nov</th>
-                            <th scope="col">Dec</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">High</th>
-                            <?php 
-                            foreach ($tripWeather["f"] as $t => $tValue):
-                                if (str_contains($t, 'max')):
-                            ?>
-                            <td>
-                                <?php echo $tValue; ?>°F / 
-                                <span style="font-size: smaller;"><?php echo $tripWeather["c"][$t]; ?>°C</span>
-                            </td>
-                            <?php 
-                                endif;
-                            endforeach;
-                            ?>
-                        </tr>
-                        <tr>
-                            <th scope="row">Low</th>
-                            <?php 
-                            foreach ($tripWeather["f"] as $t => $tValue):
-                                if (str_contains($t, 'min')):
-                            ?>
-                            <td>
-                                <?php echo $tValue; ?>°F / 
-                                <span style="font-size: smaller;"><?php echo $tripWeather["c"][$t]; ?>°C</span>
-                            </td>
-                            <?php 
-                                endif;
-                            endforeach;
-                            ?>
-                        </tr>
-                        <tr>
-                            <th scope="row">Rain</th>
-                            <?php 
-                            foreach ($tripWeather["pre"] as $p => $pValue):
-                                $cmValue = $pValue * 2.54; // Convert inches to centimeters
-                            ?>
-                            <td>
-                                <?php echo $pValue; ?>" / 
-                                <span style="font-size: smaller;"><?php echo number_format($cmValue, 2); ?> cm</span>
-                            </td>
-                            <?php 
-                            endforeach;
-                            ?>
-                        </tr>
-                        </tr>
-                    </tbody>
-                </table>
+            <table class="table table-striped">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col">Jan</th>
+                    <th scope="col">Feb</th>
+                    <th scope="col">Mar</th>
+                    <th scope="col">Apr</th>
+                    <th scope="col">May</th>
+                    <th scope="col">Jun</th>
+                    <th scope="col">Jul</th>
+                    <th scope="col">Aug</th>
+                    <th scope="col">Sep</th>
+                    <th scope="col">Oct</th>
+                    <th scope="col">Nov</th>
+                    <th scope="col">Dec</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">High</th>
+                    <?php 
+                    foreach ($tripWeather["f"] as $t => $tValue):
+                        if (str_contains($t, 'max')):
+                    ?>
+                    <td>
+                        <span class="unit-group">
+                            <?php echo $tValue; ?>°F<span class="separator">·</span>
+                            <span class="small-text"><?php echo $tripWeather["c"][$t]; ?>°C</span>
+                        </span>
+                    </td>
+                    <?php 
+                        endif;
+                    endforeach;
+                    ?>
+                </tr>
+                <tr>
+                    <th scope="row">Low</th>
+                    <?php 
+                    foreach ($tripWeather["f"] as $t => $tValue):
+                        if (str_contains($t, 'min')):
+                    ?>
+                    <td>
+                        <span class="unit-group">
+                            <?php echo $tValue; ?>°F<span class="separator">·</span>
+                            <span class="small-text"><?php echo $tripWeather["c"][$t]; ?>°C</span>
+                        </span>
+                    </td>
+                    <?php 
+                        endif;
+                    endforeach;
+                    ?>
+                </tr>
+                <tr>
+                    <th scope="row">Rain</th>
+                    <?php 
+                    foreach ($tripWeather["pre"] as $p => $pValue):
+                        $cmValue = $pValue * 2.54; // Convert inches to centimeters
+                    ?>
+                    <td>
+                        <span class="unit-group">
+                            <?php echo $pValue; ?>"<span class="separator">·</span>
+                            <span class="small-text"><?php echo number_format($cmValue, 2); ?> cm</span>
+                        </span>
+                    </td>
+                    <?php 
+                    endforeach;
+                    ?>
+                </tr>
+            </tbody>
+        </table>
             </div>
         </div>
     </div>
